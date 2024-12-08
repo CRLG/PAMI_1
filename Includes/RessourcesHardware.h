777ae8a804a296913e42659b1c7a1637eee6d7ed
NicoFarmer <nicolas.santiago77.deal@gmail.com>
2024-12-08 18:15:06 +0100

    Adaptation du fichier RessourcesHardware.h pour C et C++

--- Includes/RessourcesHardware.h
+++ Includes/RessourcesHardware.h
@@ -4,10 +4,18 @@
 #ifndef _RESSOURCES_HARDWARE_H_ 
 #define _RESSOURCES_HARDWARE_H_
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #include "main.h"
 
 void setLedBuiltin(bool state);
 void toggleLedBuiltin();
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
 
